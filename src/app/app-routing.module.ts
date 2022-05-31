b78bb44490673fc8c8dcd423e2b4d7a343b0bea9
sventuriniTL <sventurini@tecnolab.com.ar>
2022-05-31 17:07:36 -0300

    se agrega ruta de componente registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path:'',  component:LoginComponent}, 
   { path:'dashBoard', component:DashboardComponent },
+  { path:'register', component:RegisterComponent },
   
 ];
 
