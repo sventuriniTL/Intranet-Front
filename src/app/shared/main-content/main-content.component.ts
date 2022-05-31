b7f5a7167cfb11d80eac75369faab8bf13fa6a18
sventuriniTL <sventurini@tecnolab.com.ar>
2022-05-31 11:48:22 -0300

    Se agrega sistema responsive para las filas y columnas de graficos

--- src/app/shared/main-content/main-content.component.ts
+++ src/app/shared/main-content/main-content.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
+
 export class MainContentComponent implements OnInit {
+  mybreakpoint!: number;
+  cols! : number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  gridByBreakpoint = {
+    xl: 3,
+    lg: 2,
+    md: 2,
+    sm: 2,
+    xs: 1
+  }
+  constructor(private BreakpointObserver: BreakpointObserver) {
+    this.BreakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.cols = this.gridByBreakpoint.xs;
+        }
+        if (result.breakpoints[Breakpoints.Small]) {
+          this.cols = this.gridByBreakpoint.sm;
+        }
+        if (result.breakpoints[Breakpoints.Medium]) {
+          this.cols = this.gridByBreakpoint.md;
+        }
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.cols = this.gridByBreakpoint.lg;
+        }
+        if (result.breakpoints[Breakpoints.XLarge]) {
+          this.cols = this.gridByBreakpoint.xl;
+        }
+      }
+    });
   }
 
+  ngOnInit() {
+  }
+  
 }
