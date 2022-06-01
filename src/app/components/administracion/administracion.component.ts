035acd0c119c51feca9a481989677300d86b7c9d
Santiago Venturini <santyventu@gmail.com>
2022-06-01 19:20:28 -0300

    se crea componente administracion

---
+++ src/app/components/administracion/administracion.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-administracion',
+  templateUrl: './administracion.component.html',
+  styleUrls: ['./administracion.component.css']
+})
+export class AdministracionComponent implements OnInit {
+  cols! : number;
+
+  gridByBreakpoint = {
+    xl: 3,
+    lg: 2,
+    md: 2,
+    sm: 2,
+    xs: 1
+  }
+  constructor(private BreakpointObserver: BreakpointObserver) {
+    this.BreakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.cols = this.gridByBreakpoint.xs;
+        }
+        if (result.breakpoints[Breakpoints.Small]) {
+          this.cols = this.gridByBreakpoint.sm;
+        }
+        if (result.breakpoints[Breakpoints.Medium]) {
+          this.cols = this.gridByBreakpoint.md;
+        }
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.cols = this.gridByBreakpoint.lg;
+        }
+        if (result.breakpoints[Breakpoints.XLarge]) {
+          this.cols = this.gridByBreakpoint.xl;
+        }
+      }
+    });
+  }
+  ngOnInit(): void {
+  }
+
+}
