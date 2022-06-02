d805d0f02fa7640d9e8957f4feaf1617920d745e
sebastianfviola <sviola@tecnolab.com.ar>
2022-06-01 21:27:16 -0300

    login con Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +40,7 @@ import { RegisterComponent } from './components/register/register.component';
     ChartsModule,
     LayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     
   ],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,20 +11,64 @@ export class LoginComponent {
   email!: string;
   password!: string;
 
-  constructor() { }
+  forma: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+
+  });
+
+  constructor(private router: Router,
+              private fb: FormBuilder) 
+              { 
+
+                this.crearFormulario();
+
+              }
 
   userLog = {
    email: this.email,
    password: this.password
   }
   
+  crearFormulario() {
+
+    this.forma = this.fb.group({
+      email  : ['', [ Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)] ],
+      password  : ['', [ Validators.required, Validators.minLength(6) ]  ]
+
+  });
+  }
+
+
+
   login() {
-    this.userLog.email = this.email;
-    this.userLog.password = this.password;
-    
-    console.log(this.email);
-    console.log(this.password);
-    console.log(this.userLog);
+    // this.userLog.email = this.email;
+    // this.userLog.password = this.password;
+
+    if ( this.forma.invalid ){
+      // if ( this.forma.status === 'PENDING' || 'INVALID' )  {
+  
+        return Object.values( this.forma.controls ).forEach( control => {
+          
+          if ( control instanceof FormGroup ) {
+            Object.values( control.controls ).forEach( control => control.markAsTouched());
+            
+          } else {
+            control.markAsTouched();
+          }
+          
+          return this.forma.invalid;
+        });
+        
+       
+      } else  {
+        
+          console.log(this.email);
+          console.log(this.password);
+          console.log(this.userLog);
+          this.router.navigate(['/dashBoard']);
+      }
+
   }
 }
  
