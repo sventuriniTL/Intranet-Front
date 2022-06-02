c686e43a5c0470f25c828bbc78356fd22eeb9bcd
sventuriniTL <sventurini@tecnolab.com.ar>
2022-06-02 17:23:24 -0300

    cambio de nombre imagen y mail automaticamente con el loggin

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
   showFiller = false;
-
+  imgUser!: string;
+  nameUser!: string;
+  emailUser!: string;
   
-  constructor() {}
+  
+  constructor(private routes: Router) {
+   
+  }
 
   ngOnInit(): void {
+   if(this.routes.url === '/dashboardAdmin' ){
+      this.imgUser = '../../../assets/imagenes/Gustavo.png'
+      this.nameUser = 'Gustavo Auteri'
+      this.emailUser = 'gauteri@tecnolab.com.ar'
+    }
+    else if (this.routes.url === '/dashBoard'){
+      this.imgUser = '../../../assets/imagenes/MicrosoftTeams-image.png'
+      this.nameUser = 'Roberto Faivovich'
+      this.emailUser = 'rfaivovich@tecnolab.com.ar'
+    }
   }
 
 }
