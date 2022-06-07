e79756f1d8f4b0bcdf18462626eefbcc8b9d2446
sventuriniTL <sventurini@tecnolab.com.ar>
2022-06-07 17:06:15 -0300

    se agrega componente hijo powerbi2

--- src/app/components/dashboard1/inicio/inicio.component.ts
+++ src/app/components/dashboard1/inicio/inicio.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
+  providers:[NgbCarouselConfig]
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
+  images = [1, 2].map((n) => `../../assets/imagenes/slider${n}.jpg`);
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 3000;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
   }
