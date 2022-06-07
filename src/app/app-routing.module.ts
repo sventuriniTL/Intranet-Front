2fd0094d40e7c0f548c844bf6b1d85afadd71c0f
sventuriniTL <sventurini@tecnolab.com.ar>
2022-06-07 15:13:31 -0300

    carga perezosa, unicamente trae al usuario el componente indicado (fix de rendimiento)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { Dashboard1Component } from './components/dashboard1/dashboard1.component';
 import { LoginComponent } from './components/login/login.component';
-import { PowerbiComponent } from './components/powerbi/powerbi.component';
+import { PowerbiComponent } from './components/dashboard1/powerbi/powerbi.component';
 import { RegisterComponent } from './components/register/register.component';
-import { DashboardAdministraiconComponent } from './shared/dashboard-administraicon/dashboard-administraicon.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
 
 export const routes: Routes = [
-  { path: 'principal', component:SideBarComponent, 
-  children: [
-    { path:'', redirectTo: 'principal', pathMatch:'full'},
-    { path:'dashboardAdmin', component:DashboardAdministraiconComponent },
-    { path:'**', redirectTo: 'principal', pathMatch:'full'}
-  ]
-},
-{ path:'login',  component:LoginComponent, pathMatch:'full' }, 
-{ path:'register', component:RegisterComponent, pathMatch:'full'  },
-{ path:'powerBi', component:PowerbiComponent, pathMatch:'full'  },
-{ path:'', redirectTo: 'login', pathMatch:'full'},
-  
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard1', loadChildren: () => import('./components/dashboard1/dashboard1.module').then(x => x.Dashboard1Module) },
+  // { path:'dashboard1', loadChildren:() => import('./components/dashboard1/dashboard1.module').then(x => x.Dashboard1Module)},
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'powerBi', component: PowerbiComponent, pathMatch: 'full' },
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
