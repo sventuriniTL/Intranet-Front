578925ba974f1bb693dece88459ed0da8d6222c1
sventuriniTL <sventurini@tecnolab.com.ar>
2022-06-10 12:48:33 -0300

    se importan hhtpcliente y observable al servicio

--- src/app/services/clientes-con-dificultad.service.ts
+++ src/app/services/clientes-con-dificultad.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesConDificultadService {
+  readonly clientesCDApiUrl = "https://localhost:7290/api";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getClientesCDList(): Observable<any[]> {
+    return this.http.get<any>(this.clientesCDApiUrl + '/sp_CLIENTES_CON_DIFICULTADESLoadAllResult');
+  }
+
+  addClientesCD(data: any) {
+    return this.http.post(this.clientesCDApiUrl + '/sp_CLIENTES_CON_DIFICULTADESLoadAllResult', data);
+  }
+
+  deleteClientesCD(data: any) {
+    return this.http.delete(this.clientesCDApiUrl + '/sp_CLIENTES_CON_DIFICULTADESLoadAllResult', data);
+  }
 }
